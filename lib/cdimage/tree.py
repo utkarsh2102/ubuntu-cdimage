af087c200ae2781c128355b11c6b08847e9c958c
Łukasz 'sil2100' Zemczak <lukasz.zemczak@canonical.com>
2020-04-24 17:20:56 +0200

    Fix indentation of some older commit to fix tests.

--- lib/cdimage/tree.py
+++ lib/cdimage/tree.py
@@ -1794,7 +1794,7 @@ class DailyTreePublisher(Publisher):
             else:
                 return 1.2 * 1000 * 1000 * 1000
         elif self.project == "ubuntu-mate" and self.config["DIST"] >= "focal":
-                return int(2.8 * 1000 * 1000 * 1000)
+            return int(2.8 * 1000 * 1000 * 1000)
         elif (self.project in ("ubuntu-budgie", "mythbuntu", "xubuntu",
                                "ubuntu-gnome", "ubuntu-mate") and
               self.config["DIST"] >= "xenial"):
