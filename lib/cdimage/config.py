811b9a16eb943bb5e9d64acad5d4ed26b7005340
Łukasz 'sil2100' Zemczak <lukasz.zemczak@canonical.com>
2021-01-21 14:15:57 +0100

    Update focal to 20.04.2

--- lib/cdimage/config.py
+++ lib/cdimage/config.py
@@ -201,7 +201,7 @@ all_series.extend([
         pointversion="19.10.1"),
     Series(
         "focal", "20.04", "Focal Fossa",
-        pointversion="20.04.1",
+        pointversion="20.04.2",
         all_lts_projects=True),
     Series("groovy", "20.10", "Groovy Gorilla"),
     Series("hirsute", "21.04", "Hirsute Hippo"),
