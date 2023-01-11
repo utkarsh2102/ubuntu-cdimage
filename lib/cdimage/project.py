fe6eb1b4d3ce2d103b6338ef80da0393b2b6911b
Sean Davis <sean@bluesabre.org>
2023-01-11 06:53:19 -0500

    Prefer "Xubuntu Minimal" over "Xubuntu-Minimal"

--- lib/cdimage/project.py
+++ lib/cdimage/project.py
@@ -32,7 +32,7 @@ project_map = {
     "ubuntu-zh_CN": "Ubuntu Chinese Edition",
     "kubuntu": "Kubuntu",
     "xubuntu": "Xubuntu",
-    "xubuntu-minimal": "Xubuntu-Minimal",
+    "xubuntu-minimal": "Xubuntu Minimal",
     "gobuntu": "Gobuntu",
     "ubuntu-server": "Ubuntu-Server",
     "ubuntustudio": "Ubuntu-Studio",
