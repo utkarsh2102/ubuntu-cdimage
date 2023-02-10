da11d71cdd34bfceb3ed81b7fc5d1c2e9c43e495
Łukasz 'sil2100' Zemczak <lukasz.zemczak@canonical.com>
2023-02-10 12:45:47 +0100

    Update jammy to 22.04.2

--- lib/cdimage/config.py
+++ lib/cdimage/config.py
@@ -245,7 +245,7 @@ all_series.extend([
     Series("impish", "21.10", "Impish Indri"),
     Series(
         "jammy", "22.04", "Jammy Jellyfish",
-        pointversion="22.04.1",
+        pointversion="22.04.2",
         all_lts_projects=True,
         _core_series="22"),
     Series("kinetic", "22.10", "Kinetic Kudu"),
