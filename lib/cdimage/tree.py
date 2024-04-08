6e43ad0cbb4fb3e9aa56f8ddf37d29b88caab4a0
Simon Quigley <tsimonq2@lubuntu.me>
2024-04-08 08:55:39 -0500

    Lubuntu no longer builds focal images
    Remove the corresponding conditional and bump the else switch to 2.0 GB, which
    aligns with our expectations since Focal's release

--- lib/cdimage/tree.py
+++ lib/cdimage/tree.py
@@ -1896,12 +1896,8 @@ class DailyTreePublisher(Publisher):
         elif self.project == "lubuntu" and self.config["DIST"] >= "jammy":
             # Per IRC discussions on #ubuntu-release 2023-11-13
             return int(3.1 * 1000 * 1000 * 1000)
-        elif self.project == "lubuntu" and self.config["DIST"] >= "focal":
-            # https://bugs.launchpad.net/bugs/1796368
-            return int(2 * 1000 * 1000 * 1000)
         elif self.project == "lubuntu":
-            # https://irclogs.ubuntu.com/2017/07/27/%23ubuntu-release.html#t23:05
-            return int(1.5 * 1000 * 1000 * 1000)
+            return int(2.0 * 1000 * 1000 * 1000)
         elif self.project == "ubuntu-server":
             if self.config["DIST"] >= "jammy":
                 # Our images have been >2GB for quite some time now, and nobody
